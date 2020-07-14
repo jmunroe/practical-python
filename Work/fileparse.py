821969c26dcc5da63f6fc8b7e223b665330a5200
James Munroe <jmunroe@mun.ca>
2020-07-14 09:54:37 -0400

    exercise 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,33 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename, select=None):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+        
+        records = []
+        for row in rows:
+            if not row:    # Skip rows with no data
+                continue
+
+            if indices:
+                row = [ row[index] for index in indices]
+                
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
